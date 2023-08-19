54561314ec6f822eef4d6af92952a21317684bf9
MargaretTechworld <margarettechworld@gmail.com>
2023-08-19 00:48:09 +0000

    add <NewBookForm> components that contains a form to add books

---
+++ src/components/NewBookForm.js
@@ -0,0 +1,37 @@
+import Button from './Button';
+
+const formStyle = {
+  display: 'flex',
+  gap: '10px',
+};
+const bookInput = {
+  width: '30%',
+  padding: '0.5%',
+};
+
+const bookAuthor = {
+  width: '10%',
+  padding: '0.5%',
+
+};
+function NewBookForm() {
+  return (
+    <form style={formStyle}>
+      <input
+        style={bookInput}
+        type="text"
+        placeholder="BookTitle"
+      />
+      <input
+        style={bookAuthor}
+        type="text"
+        placeholder="BookAuthor"
+      />
+      <Button
+        type="submit"
+        value="Add"
+      />
+    </form>
+  );
+}
+export default NewBookForm;
