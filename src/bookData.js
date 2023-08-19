75f5f82231262cefb53a102ed5d5ac57c0978330
MargaretTechworld <margarettechworld@gmail.com>
2023-08-19 00:46:40 +0000

    add an array of book data

---
+++ src/bookData.js
@@ -0,0 +1,28 @@
+const bookData = [
+  {
+    bookId: 1,
+    bookTitle: 'History Maker',
+    author: 'Dr. Cindy Trimm',
+
+  },
+  {
+    bookId: 2,
+    bookTitle: 'The Purpose Driven Life',
+    author: 'Wrick Warren',
+
+  },
+  {
+    bookId: 3,
+    bookTitle: 'Providence',
+    author: 'John Piper',
+
+  },
+  {
+    bookId: 4,
+    bookTitle: 'Case for Grace for Kids',
+    author: 'Lee StrobelL',
+
+  },
+];
+
+export default bookData;
