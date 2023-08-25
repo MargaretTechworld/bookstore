a5b82210a483e5b0bd033171c149bee34ec74f1a
MargaretTechworld <margarettechworld@gmail.com>
2023-08-25 22:49:21 +0000

    style routes

--- src/routes/RootLayout.js
+++ src/routes/RootLayout.js
@@ -1,32 +1,13 @@
 import { NavLink, Outlet } from 'react-router-dom';
 
-const header = {
-  display: 'flex',
-  alignItems: 'center',
-  paddingTop: ' 1%',
-  paddingLeft: '5%',
-  paddingRight: '5%',
-  marginBottom: '4%',
-};
-const nav = {
-  padding: '0 10px',
-  fontSize: '1.2rem',
-  textDecoration: 'none',
-};
-const logo = {
-  color: '#0290FF',
-};
-
 function RootLayout() {
   return (
     <div>
-      <div style={header}>
-        <h1 style={logo}>Bookstore CMS</h1>
-        <div>
-          <NavLink style={nav} to="/">Books</NavLink>
-          <NavLink style={nav} to="category">Categories</NavLink>
-        </div>
-      </div>
+      <header>
+        <h1 className="logo">Bookstore CMS</h1>
+        <NavLink className="nav books" to="/">BOOKS</NavLink>
+        <NavLink className="nav category" to="category">CATEGORIES</NavLink>
+      </header>
       <main>
         <Outlet />
       </main>
