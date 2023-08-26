615e911c788585095905f185ab888407e59aeb70
MargaretTechworld <margarettechworld@gmail.com>
2023-08-26 07:33:16 +0000

    made changes required

--- src/routes/RootLayout.js
+++ src/routes/RootLayout.js
@@ -1,12 +1,18 @@
+import { FaUser } from 'react-icons/fa';
 import { NavLink, Outlet } from 'react-router-dom';
 
 function RootLayout() {
   return (
     <div>
       <header>
-        <h1 className="logo">Bookstore CMS</h1>
-        <NavLink className="nav books" to="/">BOOKS</NavLink>
-        <NavLink className="nav category" to="category">CATEGORIES</NavLink>
+        <div className="nav-container">
+          <h1 className="logo">Bookstore CMS</h1>
+          <NavLink className="nav books" to="/">BOOKS</NavLink>
+          <NavLink className="nav category" to="category">CATEGORIES</NavLink>
+        </div>
+        <div>
+          <FaUser className="user" size={14} />
+        </div>
       </header>
       <main>
         <Outlet />
